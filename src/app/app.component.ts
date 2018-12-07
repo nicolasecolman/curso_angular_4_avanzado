44b0b2c5084a28828087c81428fee5f2c60e4ffd
nicolasecolman <nicolasecolman@gmail.com>
2018-12-07 16:38:32 -0300

    Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { SpeechRecognitionService } from './services/speech-recognition.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngspeech';
+export class AppComponent implements OnInit, OnDestroy {
+  showSearchButton: boolean;
+  speechData: string;
+
+  constructor(private speechRecognitionService: SpeechRecognitionService) {
+      this.showSearchButton = true;
+      this.speechData = "";
+  }
+
+  ngOnInit() {
+      console.log("hello")
+  }
+
+  ngOnDestroy() {
+      this.speechRecognitionService.DestroySpeechObject();
+  }
+
+  activateSpeechSearchMovie(): void {
+      this.showSearchButton = false;
+
+      this.speechRecognitionService.record()
+          .subscribe(
+          //listener
+          (value) => {
+              this.speechData = value;
+              console.log(value);
+          },
+          //errror
+          (err) => {
+              console.log(err);
+              if (err.error == "no-speech") {
+                  console.log("--restatring service--");
+                  this.activateSpeechSearchMovie();
+              }
+          },
+          //completion
+          () => {
+              this.showSearchButton = true;
+              console.log("--complete--");
+              this.activateSpeechSearchMovie();
+          });
+  }
+
 }
